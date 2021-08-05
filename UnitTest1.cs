d6c52a76a2029b4cb5c19183fc814b65fc567b95
Serhii Spasennikov <sergeys@wizardsdev.com>
2021-08-05 11:52:49 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var reguest = new RestRequest("posts/{postid}", Method.GET);
+            reguest.AddUrlSegment("postid", 1);
+
+            var responce = client.Execute(reguest);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(responce);
+
+            var result = output["author"];
+
+            Assert.That(result, Is.EqualTo("typicode"), "Erros author name ");
+        }
+    }
+}
